52082bba101facf385d284f61d6ce0a79ec49826
AmraRam <43521531+amramanoj@users.noreply.github.com>
2023-06-07 22:21:12 +0530

    Update Java.java

--- src/Java.java
+++ src/Java.java
@@ -5,7 +5,7 @@ public class Java extends Tcg{
 
     public void setvalue(){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter usfdsfsdehhgrgfggf name");
+        System.out.println("Enter usfdsfsdehghhgrgfggf name");
         String name_user=sc.nextLine();
 
         System.out.println("Enter user code");
